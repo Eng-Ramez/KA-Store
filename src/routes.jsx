80d664d19e28586ccf98edc5d440ac13f71696aa
Ramez Balbisi <ramezbalbisi55@gmail.com>
2025-05-21 17:13:24 +0300

    start project and create Routing

---
+++ src/routes.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "./layout/MainLayout";
+import Error from "./pages/error/Error";
+import Home from "./pages/home/Home";
+import Shop from "./pages/shop/Shop";
+import Cart from "./pages/cart/Cart";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+const routes = createBrowserRouter([{
+    path:'/',
+    element:<MainLayout/>,
+    errorElement:<Error/>,
+    children:[
+        {
+            path:'/',
+            element:<Home/>
+        },
+        {
+            path:'/shop',
+            element:<Shop/> 
+        },
+
+        {
+            path:'/cart',
+            element:<Cart/> 
+        }, 
+
+        {
+            path:'/login',
+            element:<Login/> 
+        },
+
+        {
+            path:'/register',
+            element:<Register/> 
+        },
+    
+],
+}]);
+export default routes;
